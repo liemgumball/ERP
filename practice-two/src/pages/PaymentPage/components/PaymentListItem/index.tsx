850cef9c6a5f403d2eb4b80cea29b41b57e3667d
HoangHongNgo <honghoangngo@gmail.com>
2023-12-28 21:02:28 +0700

    done payment

--- practice-two/src/pages/PaymentPage/components/PaymentListItem/index.tsx
+++ practice-two/src/pages/PaymentPage/components/PaymentListItem/index.tsx
@@ -1,8 +1,8 @@
 import Button from '@components/Button';
 import { formatDate, formatAmount } from '@services/format';
-import { TPayment } from 'src/types';
 import eye from '@assets/eye.svg';
 import { NOTIFIES_MSG } from '@constants/messages';
+import { useNavigate } from 'react-router-dom';
 
 type PaymentListItemProps = {
   payment: any;
@@ -11,10 +11,37 @@ type PaymentListItemProps = {
 const PaymentListItem = ({ payment }: PaymentListItemProps) => {
   const { id, student, course, paid, amount, paid_at } = payment;
 
+  const navigate = useNavigate();
+
   console.log(payment);
 
-  const viewDetailClick = () => {
-    alert(NOTIFIES_MSG.FUTURE_FEATURE);
+  const viewDetailClick = async () => {
+    const endpoint = `${import.meta.env.VITE_API_URL}/payments/payment/`;
+    const requestBody = {
+      order_id: id.toString(),
+      order_type: 'pay',
+      amount: amount,
+      order_desc: `${student.name} thanh toan hoc phi mon hoc ${course.name}`,
+      bank_code: 'NCB',
+      language: 'vn',
+    };
+
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // Add any additional headers if needed
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    window.location.href = data['payment_url'];
   };
 
   return (
@@ -24,7 +51,7 @@ const PaymentListItem = ({ payment }: PaymentListItemProps) => {
       <p className="truncate">{course?.name}</p>
       <p className="truncate normal-case">{paid ? 'Paid' : 'Not yet'}</p>
       <p className="truncate uppercase">inr {formatAmount(amount)}</p>
-      <p className="truncate uppercase">{paid_at}</p>
+      <p className="truncate uppercase">{formatDate(paid_at)}</p>
       <div className="action-group flex justify-end gap-x-2">
         <Button
           className="view-details-btn group-hover:bg-custom-light-pink group-hover:hover:bg-white"
